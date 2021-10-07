a74cdf5096cbaad72de636e98e9315cf1faebe60
MFaiqKhan <faiqkhanthinkcentre@yahoo.com>
2021-10-07 19:40:16 +0500

    Added directories of classes and time

---
+++ Class 1 (4-10-21)/Variable-class-1.js
@@ -0,0 +1,12 @@
+console.log('Hello world');
+//script = limited work
+//common js(vanilla javascript)
+// we will study AMD js in 4 weeks
+// resource we will be using is w3schools 
+// we can search javascript variables on w3schools.
+//---content----
+//variables:
+//data type: number, js have 64bit memory number only . 
+// you have to talk in prog lang, like words are called string. so we will say strings not words 
+//or text when talking.
+// "let" is use instead of var in js was released in ecma6
